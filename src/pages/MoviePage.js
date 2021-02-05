4e4886e54904ef8e17ddd730cae4c90886ffa795
Ylva Nilsson <ylva.z.nilsson@teliacompany.com>
2021-02-05 09:12:55 +0100

    updated path MovieDetils

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { Loading } from "../components/Loading";
-import { MovieDetails } from "./MovieDetails";
+import { MovieDetails } from "../components/MovieDetails";
 import { NotFound } from "./NotFound";
 
 export const MoviePage = () => {
