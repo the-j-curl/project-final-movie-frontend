e6d86b6606cb2e3f129f2067913ae13c33115d00
Jamie Cook <jamie.p.cook@outlook.com>
2021-02-06 09:25:53 +0100

    Not Found page styled

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,48 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components/macro";
+
+import { BackButton } from "../components/BackButton";
 
 export const NotFound = () => {
-  return <h1>Page Not Found</h1>;
+  const history = useHistory();
+
+  return (
+    <ErrorMessage>
+      <BackButton />
+      <Heading>404 - Page Not Found</Heading>
+      <ErrorText>
+        Ooops, something went wrong - Please try again later
+      </ErrorText>
+    </ErrorMessage>
+  );
 };
+
+const ErrorMessage = styled.main`
+  height: 66vh;
+  /* background-image: url("../images/cinema-image.jpg"); */
+  background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(32, 32, 32, 0.5)),
+    url("../images/cinema-image.jpg");
+  background-size: cover;
+  background-position-x: center;
+  background-position-y: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const Heading = styled.h3`
+  @media (min-width: 1024px) {
+    font-size: 28px;
+  }
+`;
+
+const ErrorText = styled.p`
+  font-size: 16px;
+
+  @media (min-width: 1024px) {
+    font-size: 20px;
+  }
+`;
